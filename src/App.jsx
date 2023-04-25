a0c8656d66be9fe5427db95765a98b5efac315af
Rodrigopm98 <rodrigo.rp885@gmail.com>
2023-04-24 21:18:49 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import './App.css';
+import { TodoAdd } from './components/TodoAdd';
+import { TodoList } from './components/TodoList';
+import { useTodo } from './hooks/useTodo';
+
+function App() {
+	const {
+		todos,
+		todosCount,
+		pendingTodosCount,
+		handleNewTodo,
+		handleDeleteTodo,
+		handleCompleteTodo,
+		handleUpdateTodo,
+	} = useTodo();
+
+	return (
+		<>
+			<div className='card-to-do'>
+				<h1>Lista de Tareas</h1>
+				<div className='add-todo'>
+					<TodoAdd handleNewTodo={handleNewTodo} />
+				</div>
+        <div className='counter-todos'>
+					<h3>
+						N° Tareas: <span>{todosCount}</span>
+					</h3>
+					<h3>
+						Pendientes: <span>{pendingTodosCount}</span>
+					</h3>
+				</div>
+
+				<TodoList
+					todos={todos}
+					handleUpdateTodo={handleUpdateTodo}
+					handleDeleteTodo={handleDeleteTodo}
+					handleCompleteTodo={handleCompleteTodo}
+				/>
+			</div>
+		</>
+	);
+}
+
+export default App;
